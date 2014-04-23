8ebee47878b664d9ede0a680305d2ddd50fe2835
Kevin Shi <xpspectre@gmail.com>
2014-04-22 20:37:44 -0400

    Get complete lists of unique patient mutations and mutations that are also found in HPRD.

--- cosmic_parsing/process_data_hotnet.m
+++ cosmic_parsing/process_data_hotnet.m
@@ -7,6 +7,8 @@ load CosmicIncFus_samples_multi
 load hotnetgenes
 
 patient_data = {};
+all_patient_genes = {}; % list of all patient mutations
+acccepted_patient_genes = {}; % list of patient mutations in HPRD
 for i = 1:size(samples_multi,1)
     name = samples_multi{i,1};
     tab = samples_multi{i,2};
@@ -16,12 +18,20 @@ for i = 1:size(samples_multi,1)
         pos = find(strcmp(gene,hotnetgenes));
         if ~isempty(pos) % only take genes listed in hotnet
             mutations = [mutations; gene];
+            acccepted_patient_genes = [acccepted_patient_genes; gene];
         end
+        all_patient_genes = [all_patient_genes; gene];
     end
     patient_data = [patient_data; {name, mutations}];
 end
 
 save patient_data_multi patient_data
+
+all_patient_genes = unique(all_patient_genes);
+save all_patient_genes all_patient_genes
+
+acccepted_patient_genes = unique(acccepted_patient_genes);
+save acccepted_patient_genes all_patient_genes
 
 %% Convert patient data matrix into format for hotnet
 f = fopen('patient_mutations_multi.txt','w');
